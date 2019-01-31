36d6bb495201d1d5080208a645993c74c0a61b10
Ananta Pratama <rinna@Anantas-MacBook-Pro.local>
2019-01-31 13:12:21 +0800

    Added tab bar icons

--- src/screens/Home/Jobs/Jobs.tsx
+++ src/screens/Home/Jobs/Jobs.tsx
@@ -1,8 +1,35 @@
 import React, { Component } from "react"
-import { View, StyleSheet, Text } from "react-native"
+import { View, StyleSheet, Text, Image } from "react-native"
 import FloatingActionButton from "../../../components/FloatingActionButton"
+import { NavigationTabScreenOptions } from "react-navigation"
+
+const IC_JOBS_ACTIVE = require("../../../../assets/ic_jobs_active.png")
+const IC_JOBS_INACTIVE = require("../../../../assets/ic_jobs_inactive.png")
 
 export default class Jobs extends Component {
+  static navigationOptions: NavigationTabScreenOptions = {
+    tabBarIcon: ({ focused }) => {
+      switch (focused) {
+        case true:
+          return (
+            <Image
+              source={IC_JOBS_ACTIVE}
+              style={{ width: 20, height: 20 }}
+              resizeMode={"contain"}
+            />
+          )
+        case false:
+          return (
+            <Image
+              source={IC_JOBS_INACTIVE}
+              style={{ width: 20, height: 20 }}
+              resizeMode={"contain"}
+            />
+          )
+      }
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
--- src/screens/Home/SAR/SAR.tsx
+++ src/screens/Home/SAR/SAR.tsx
@@ -1,7 +1,34 @@
 import React, { Component } from "react"
-import { View, StyleSheet, Text } from "react-native"
+import { View, StyleSheet, Text, Image } from "react-native"
+import { NavigationTabScreenOptions } from "react-navigation"
+
+const IC_BOOK_ACTIVE = require("../../../../assets/ic_book_active.png")
+const IC_BOOK_INACTIVE = require("../../../../assets/ic_book_inactive.png")
 
 export default class SAR extends Component {
+  static navigationOptions: NavigationTabScreenOptions = {
+    tabBarIcon: ({ focused }) => {
+      switch (focused) {
+        case true:
+          return (
+            <Image
+              source={IC_BOOK_ACTIVE}
+              style={{ width: 20, height: 20 }}
+              resizeMode={"contain"}
+            />
+          )
+        case false:
+          return (
+            <Image
+              source={IC_BOOK_INACTIVE}
+              style={{ width: 20, height: 20 }}
+              resizeMode={"contain"}
+            />
+          )
+      }
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
